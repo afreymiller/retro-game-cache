111ba6294a200e1ff5fe03566a98f73247a5ec25
Adam Freymiller <afreymiller@Adams-Air.attlocal.net>
2021-04-05 21:28:28 -0500

    adding searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" placeholder="Search over 80 video games..."/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
